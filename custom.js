24d9268e33767e66d02bb72e022014678ace2d19
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-04-26 01:39:24 -0700

    added color picker, sidebar minimization and random color functionalities

--- custom.js
+++ custom.js
@@ -1,15 +1,72 @@
+const bgs = ['nav-bg-color', 'card-bg-color', 'front-bg-color', 'middle-bg-color', 'btn-bg-color'];
+const clr = ['nav-fg-color', 'card-fg-color', 'front-fg-color', 'btn-fg-color'];
+
+function randomize() {
+	for (var i=0; i<bgs.length; i++) {
+		$('.' + bgs[i]).css('background-color', randomHex());
+	}
+	for (i=0; i<clr.length; i++) {
+		$('.' + clr[i]).css('color', randomHex());
+	}
+}
+
+function randomHex() {
+	return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+var sidebarShown = true;
+function toggleSidebar() {
+	if (sidebarShown == true) hideSidebar();
+	else showSidebar();
+}
+
+function hideSidebar() {
+	$("body").css('margin-left', '0');
+	$(".sidebar").css('left', '-310px');
+	sidebarShown = false;
+}
+
+function showSidebar() {
+	$("body").css('margin-left', '300px');
+	$(".sidebar").css('left', '0px');
+	sidebarShown = true;
+}
+
 $(document).ready(() => {
-	const bgs = ['nav-bg-color', 'card-bg-color', 'front-bg-color', 'middle-bg-color', 'btn-bg-color'];
-	const clr = ['nav-fg-color', 'card-fg-color', 'front-fg-color', 'btn-fg-color'];
 
 	// Listen for color changes
 	for (var i=0; i<bgs.length; i++) {
-		$('#' + bgs[i]).on('input',function(e){
-			$('.' + e.target.id).css('background-color', '#' + $(e.target).val());
-  	});
-		$('#' + clr[i]).on('input',function(e){
-			$('.' + e.target.id).css('color', '#' + $(e.target).val());
-  	});
+		$('#' + bgs[i]).spectrum({ 
+			showInput: true,
+			preferredFormat: "hex",
+			allowEmpty:true,
+    		showInitial: true,
+    		showInput: true
+		});
+		$('#' + bgs[i]).on('move.spectrum',function(e, tinycolor){
+			$('.' + e.target.id).css('background-color', tinycolor.toHexString());
+  		});
+  	}
+  	for (i=0; i<clr.length; i++) {
+		$('#' + clr[i]).spectrum({ 
+			showInput: true,
+			preferredFormat: "hex",
+			allowEmpty:true,
+		    showInitial: true,
+		    showInput: true
+		});
+		$('#' + clr[i]).on('move.spectrum',function(e, tinycolor){
+			$('.' + e.target.id).css('color', tinycolor.toHexString());
+  		});
 	}
 
+
+	$("#random").click(function(e) {
+		randomize();
+	});
+
+	$("#sidebar").click(function(e) {
+		toggleSidebar();
+	})
 });
+
