181cad2116c509eb813963a45522d995829b6290
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-04-25 23:10:12 -0700

    Add color changing website

---
+++ custom.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+	const bgs = ['nav-bg-color', 'card-bg-color', 'front-bg-color', 'middle-bg-color', 'btn-bg-color'];
+	const clr = ['nav-fg-color', 'card-fg-color', 'front-fg-color', 'btn-fg-color']
+
+	for (var i=0; i<bgs.length; i++) {
+
+	}
+
+	// Listen for color changes
+	for (var i=0; i<bgs.length; i++) {
+
+		$('#' + bgs[i]).on('input',function(e){
+			$('.' + e.target.id).css('background-color', '#' + $(e.target).val());
+    	});
+		$('#' + clr[i]).on('input',function(e){
+			$('.' + e.target.id).css('color', '#' + $(e.target).val());
+    	});
+	}
+
+});
